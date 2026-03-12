37fa1ae6271ccb803ae2af2a31e6aca8e0ec11be
ewilly <6552172+ewilly@users.noreply.github.com>
2026-03-12 08:15:32 +0100

    fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,12 +5,15 @@
 #=================================================
 
 myynh_install() {
+	# Install with npm
 	pushd $install_dir
 		ynh_hide_warnings ynh_exec_as_app npm install
 		ynh_hide_warnings ynh_exec_as_app npm run build
 	popd
 
-	mkdir -p $data_dir/uploads
+	# Create needed directories
+	mkdir -p "/var/log/$app"
+	mkdir -p "$data_dir/uploads"
 }
 
 # Set permissions
@@ -22,4 +25,8 @@ myynh_set_permissions () {
 	chown -R $app: "$data_dir"
 	chmod u=rwx,g=rx,o= "$data_dir"
 	chmod -R o-rwx "$data_dir"
+
+	chown -R $app: "/var/log/$app"
+	chmod u=rwx,g=rx,o= "/var/log/$app"
+	chmod -R o-rwx "/var/log/$app"
 }
